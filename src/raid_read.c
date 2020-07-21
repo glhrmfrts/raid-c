bfd8a5045cbc26b7defe37ec6d63d732dcafbd81
Guilherme Nemeth <guilherme.nemeth@gmail.com>
2020-07-21 00:20:20 -0300

    safe check for read functions

--- src/raid_read.c
+++ src/raid_read.c
@@ -13,6 +13,8 @@ void raid_read_init(raid_client_t* cl, msgpack_object* header, msgpack_object* b
 
 bool raid_is_code(raid_client_t* cl, const char* code)
 {
+    if (!cl->in_reader.header) return false;
+
     for (int i = 0; i < cl->in_reader.header->via.map.size; i++) {
         if (!strncmp("code", cl->in_reader.header->via.map.ptr[i].key.via.str.ptr, 4)) {
             return !strncmp(code, cl->in_reader.header->via.map.ptr[i].val.via.str.ptr, strlen(code));
@@ -23,6 +25,8 @@ bool raid_is_code(raid_client_t* cl, const char* code)
 
 bool raid_read_code(raid_client_t* cl, char** res, size_t* len)
 {
+    if (!cl->in_reader.header) return false;
+
     for (int i = 0; i < cl->in_reader.header->via.map.size; i++) {
         const char* ptr = cl->in_reader.header->via.map.ptr[i].val.via.str.ptr;
         size_t size = cl->in_reader.header->via.map.ptr[i].val.via.str.size;
@@ -38,6 +42,8 @@ bool raid_read_code(raid_client_t* cl, char** res, size_t* len)
 
 bool raid_read_int(raid_client_t* cl, int64_t* res)
 {
+    if (!cl->in_reader.header) return false;
+
     if (cl->in_reader.nested->type != MSGPACK_OBJECT_POSITIVE_INTEGER && cl->in_reader.nested->type != MSGPACK_OBJECT_NEGATIVE_INTEGER)
         return false;
 
@@ -47,6 +53,8 @@ bool raid_read_int(raid_client_t* cl, int64_t* res)
 
 bool raid_read_float(raid_client_t* cl, double* res)
 {
+    if (!cl->in_reader.header) return false;
+
     if (cl->in_reader.nested->type != MSGPACK_OBJECT_FLOAT)
         return false;
 
@@ -56,6 +64,8 @@ bool raid_read_float(raid_client_t* cl, double* res)
 
 bool raid_read_string(raid_client_t* cl, char** res, size_t* len)
 {
+    if (!cl->in_reader.header) return false;
+
     if (cl->in_reader.nested->type != MSGPACK_OBJECT_STR && cl->in_reader.nested->type != MSGPACK_OBJECT_BIN)
         return false;
 
@@ -68,6 +78,8 @@ bool raid_read_string(raid_client_t* cl, char** res, size_t* len)
 
 bool raid_read_map_key(raid_client_t* cl, char** key, size_t* len)
 {
+    if (!cl->in_reader.header) return false;
+
     if (!cl->in_reader.parent || cl->in_reader.parent->type != MSGPACK_OBJECT_MAP)
         return false;
 
@@ -81,6 +93,8 @@ bool raid_read_map_key(raid_client_t* cl, char** key, size_t* len)
 
 bool raid_read_begin_array(raid_client_t* cl, size_t* len)
 {
+    if (!cl->in_reader.header) return false;
+
     if (cl->in_reader.nested->type != MSGPACK_OBJECT_ARRAY)
         return false;
 
@@ -95,12 +109,16 @@ bool raid_read_begin_array(raid_client_t* cl, size_t* len)
 
 void raid_read_end_array(raid_client_t* cl)
 {
+    if (!cl->in_reader.header) return;
+
     cl->in_reader.nested_top--;
     cl->in_reader.nested = cl->in_reader.parents[cl->in_reader.nested_top];
 }
 
 bool raid_read_begin_map(raid_client_t* cl, size_t* len)
 {
+    if (!cl->in_reader.header) return false;
+
     if (cl->in_reader.nested->type != MSGPACK_OBJECT_MAP)
         return false;
 
@@ -115,12 +133,16 @@ bool raid_read_begin_map(raid_client_t* cl, size_t* len)
 
 void raid_read_end_map(raid_client_t* cl)
 {
+    if (!cl->in_reader.header) return;
+
     cl->in_reader.nested_top--;
     cl->in_reader.nested = cl->in_reader.parents[cl->in_reader.nested_top];
 }
 
 bool raid_read_next(raid_client_t* cl)
 {
+    if (!cl->in_reader.header) return false;
+
     if (cl->in_reader.parent->type == MSGPACK_OBJECT_ARRAY) {
         int* idx = &cl->in_reader.indices[cl->in_reader.nested_top];
         *idx += 1;
