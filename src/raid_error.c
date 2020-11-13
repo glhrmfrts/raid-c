4bd1c5407093452191a31d7b95a8271745492ff1
Guilherme Nemeth <guilherme.nemeth@gmail.com>
2020-11-13 15:52:30 -0300

    improve error messages

--- src/raid_error.c
+++ src/raid_error.c
@@ -4,26 +4,26 @@ const char* raid_error_to_string(raid_error_t err)
 {
     switch (err) {
     case RAID_SUCCESS:
-        return "success";
+        return "RAID_SUCCESS: success";
     case RAID_INVALID_ARGUMENT:
-        return "invalid argument";
+        return "RAID_INVALID_ARGUMENT: invalid argument";
     case RAID_INVALID_ADDRESS:
-        return "invalid address";
+        return "RAID_INVALID_ADDRESS: invalid host address";
     case RAID_CONNECT_ERROR:
-        return "connect error";
+        return "RAID_CONNECT_ERROR: error connecting to host";
     case RAID_SOCKET_ERROR:
-        return "socket error";
+        return "RAID_SOCKET_ERROR: error opening socket file descriptor";
     case RAID_RECV_TIMEOUT:
-        return "recv timeout";
+        return "RAID_RECV_TIMEOUT: timed out waiting data from server";
     case RAID_NOT_CONNECTED:
-        return "not connected";
+        return "RAID_NOT_CONNECTED: not connected to host";
     case RAID_SHUTDOWN_ERROR:
-        return "shutdown error";
+        return "RAID_SHUTDOWN_ERROR: invalid socket state";
     case RAID_CLOSE_ERROR:
-        return "close error";
+        return "RAID_CLOSE_ERROR: invalid socket file descriptor";
     case RAID_UNKNOWN:
-        return "unknown";
+        return "RAID_UNKNOWN: unknown error";
     default:
-        return "unlikely error";
+        return "unmapped error";
     }
 }
